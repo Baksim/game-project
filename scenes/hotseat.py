00ec6eefa8ca4b0deffa6af30c8c87ef09738b3c
edwx <kanaita14@gmail.com>
2022-05-26 21:04:32 +0300

    progress on room play

--- scenes/hotseat.py
+++ scenes/hotseat.py
@@ -6,9 +6,8 @@ from .components.chessboard_drawer import ChessboardDrawer
 import pygame_widgets
 from pygame_widgets.textbox import TextBox
 import chess
-import chess.engine
 
-def get_let(board, is_white, x, b_keys):    
+def get_let(board, is_white, x, b_keys):
     if pygame.mouse.get_focused() != 0:
         index = (x - board.x) // (board.width // 8)
         if -1 < index < len(b_keys):
@@ -19,8 +18,8 @@ def get_let(board, is_white, x, b_keys):
                 return b_keys[index]
         return None
 
-def get_num(board, is_white, y): 
-    if pygame.mouse.get_focused() != 0:  
+def get_num(board, is_white, y):
+    if pygame.mouse.get_focused() != 0:
         if board.top <= y <= board.bottom:
             if is_white:
                 return 8 - (y - board.top) // (board.width // 8)
@@ -38,13 +37,9 @@ def outcome_loop(outcome):
     while True:
         print(f"The outcome is {outcome}")
 
-def run(game, difficulty, is_white):
+def run(game):
     player_move = ""
-    def set_move(f, player_move):
-        player_move = move_filed.getText()
-        f.setText("")
     running = True
-    engine = game.get_engine()
     cboard = CBoard()
     board = chess.Board()
     fields = {
@@ -57,59 +52,44 @@ def run(game, difficulty, is_white):
         "g": [None, None, None, None, None, None, None, None],
         "h": [None, None, None, None, None, None, None, None]
     }
-    engine.configure({"Skill Level": difficulty})
-    player_turn = is_white
+    player_turn = True
+    is_white = True
     cd = ChessboardDrawer(game, is_white)
     cd.draw(cboard, fields)
-    if eval(game.settings["keyboard_moves"]):
-        move_filed = TextBox(game.screen, 500, 100, 800, 50, font=game.fonts["small_text"], fontSize=50, textColour=(0, 0, 0),
-                  onSubmit=set_move, radius=20, borderThickness=5)
-        move_filed.onSubmitParams=(move_filed, player_move)
-        size_x, size_y = game.screen.get_size() 
-        move_filed.setWidth(int(size_x // 3))
-        move_filed.setX(int(size_x // 2 - move_filed.getWidth() // 2))
-        move_filed.setY(int(size_y * 0.92))
     winner = None
     outcome = None
-    
+
     while running:
         game.screen.fill(game.colors["main"])
         events = pygame.event.get()
         pygame_widgets.update(events)
         for event in events:
-            let  = get_let(cd.rect, is_white, pygame.mouse.get_pos()[0], list(fields.keys()))
+            let = get_let(cd.rect, is_white, pygame.mouse.get_pos()[0], list(fields.keys()))
             num = get_num(cd.rect, is_white, pygame.mouse.get_pos()[1])
             game.event_handler(event)
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                if let and num and (let + str(num) != player_move) and player_turn:
+                if let and num and (let + str(num) != player_move):
                     player_move += let + str(num)
             if event.type == VIDEORESIZE or event.type == VIDEOEXPOSE:
                 cd.resize()
-                size_x, size_y = game.screen.get_size() 
-                move_filed.setWidth(int(size_x // 3))
-                move_filed.setX(int(size_x // 2 - move_filed.getWidth() // 2))
-                move_filed.setY(int(size_y * 0.92))
-    
-        if player_turn:
-            if len(player_move) >= 4:
-                if cboard.is_promotion():
-                    promote_loop(cboard)
-                player_move = chess.Move.from_uci(player_move)
-                if player_move in board.legal_moves:
-                    board.push(player_move)
-                    cboard.push(str(player_move))
-                    player_turn = not player_turn
-                player_move = ""
-        else:
-            result = engine.play(board, chess.engine.Limit(time=0))
-            board.push(result.move)
-            cboard.push(str(result.move))
-            player_turn = not player_turn
+                size_x, size_y = game.screen.get_size()
 
-        cd.draw(cboard, fields, (player_move if len(player_move) == 2 and player_turn else False))
+        if len(player_move) >= 4:
+            if cboard.is_promotion():
+                promote_loop(cboard)
+            player_move = chess.Move.from_uci(player_move)
+            if player_move in board.legal_moves:
+                board.push(player_move)
+                cboard.push(str(player_move))
+                player_turn = not player_turn
+            player_move = ""
+            is_white = not is_white
+            cd.is_white = is_white
+
+        cd.draw(cboard, fields, (player_move if len(player_move) == 2 else False))
         game.coursor()
         pygame.display.update()
 
@@ -124,8 +104,3 @@ def run(game, difficulty, is_white):
                 outcome = "draw"
             running = False
             outcome_loop(outcome)
-
-
-    if eval(game.settings["keyboard_moves"]):
-        move_filed.hide()
-        move_filed.disable()
