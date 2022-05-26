288f34c1cc6cfdc73dbda0e27a286100c2cf7ad2
boriszalovs1@gmail.com <boriszalovs1@gmail.com>
2022-05-26 23:27:06 +0300

    promotion

--- scenes/components/chessboard_drawer.py
+++ scenes/components/chessboard_drawer.py
@@ -48,8 +48,7 @@ class ChessboardDrawer:
             num = self.game.fonts["small_text"].render(str(i + 1), True, (0, 0, 0)) if self.is_white else self.game.fonts["small_text"].render(str(8 - i), True, (0, 0, 0))
             self.game.screen.blit(let, let.get_rect(center=(self.rect.left + self.rect.width //16 + self.rect.width //8 * i, self.rect.bottom + 11)))
             self.game.screen.blit(num, let.get_rect(center=(self.rect.left - 12, self.rect.bottom - self.rect.width //16 - self.rect.width //8 * i)))
-       
-                
+                 
     def resize(self):
         size_x, size_y = self.game.screen.get_size() 
         self.rect = pygame.Rect((size_x // 2 - (size_y // 2 - int(size_y*0.1)), int(size_y*0.1)), (int(size_y*0.8), int(size_y*0.8)))
@@ -63,3 +62,30 @@ class ChessboardDrawer:
         if picked:
             pygame.draw.rect(self.game.screen, self.game.colors["accent"], rects[picked[0]][int(picked[1]) - 1])
         self.draw_pices(cboard, rects)
+        
+    def get_promotion(self, cboard, rects):
+        while True:
+            self.game.screen.fill(self.game.colors["main"])
+            size_x, size_y = self.game.screen.get_size() 
+            picks = {
+            "q": pygame.Rect((self.under_rect.x - self.rect.width // 8 - 20, self.rect.top), (int(size_y*0.8), int(size_y*0.8))),
+            "r": pygame.Rect((self.under_rect.x - self.rect.width // 8 - 20, self.rect.top + (self.rect.width // 8)*2), (int(size_y*0.8), int(size_y*0.8))),
+            "b": pygame.Rect((self.under_rect.x - self.rect.width // 8 - 20, self.rect.top + (self.rect.width // 8)*3), (int(size_y*0.8), int(size_y*0.8))),
+            "n": pygame.Rect((self.under_rect.x - self.rect.width // 8 - 20, self.rect.top + (self.rect.width // 8)*4), (int(size_y*0.8), int(size_y*0.8)))
+            }
+            
+            for key in picks:
+                self.game.screen.blit(self.imgs["w" if self.is_white else "b"][key], picks[key].topleft)
+            for event in pygame.event.get():
+                self.game.event_handler(event)
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    for key in picks:
+                        if picks[key].collidepoint(pygame.mouse.get_pos()):
+                            print(key)
+                            return key
+                
+            self.draw_board(rects)
+            self.draw_symb(rects)
+            self.draw_pices(cboard, rects)
+            
+            self.game.coursor()
