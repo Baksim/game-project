ae296108c74b144639045ff39b944429bf8602fd
boriszalovs1@gmail.com <boriszalovs1@gmail.com>
2022-05-27 07:21:50 +0300

    update

--- scenes/matchmaking.py
+++ scenes/matchmaking.py
@@ -8,14 +8,12 @@ from .components.chessboard import CBoard
 from .components.chessboard_drawer import ChessboardDrawer
 from .components.ws_interface import WsInterface
 from .components.result_log import show
-
 # def connection(game, session_id):
 #     enableTrace(True)
 #     ws.on_open = on_open
 #     print("starting ws???")
 #     ws.run_forever()
 #     print("are you not running forever, son?")
-
 def get_let(board, is_white, x, b_keys):
     if pygame.mouse.get_focused() != 0:
         index = (x - board.x) // (board.width // 8)
@@ -26,7 +24,6 @@ def get_let(board, is_white, x, b_keys):
                 b_keys.reverse()
                 return b_keys[index]
         return None
-
 def get_num(board, is_white, y):
     if pygame.mouse.get_focused() != 0:
         if board.top <= y <= board.bottom:
@@ -35,10 +32,6 @@ def get_num(board, is_white, y):
             else:
                 return (y - board.top) // (board.width // 8) + 1
         return None
-
-def outcome_loop(outcome):
-    while True:
-        print(f"The outcome is {outcome}")
 
 def run(game):
     session_id = game.session_id
@@ -49,33 +42,19 @@ def run(game):
     wst = Thread(target=ws.ws.run_forever)
     wst.daemon = True
     wst.start()
-
     pygame.time.delay(2000)
     try:
         ws.match()
     except WebSocketConnectionClosedException:
         pygame.time.delay(2000)
         ws.match()
-
-    title = game.fonts["text"].render("Waiting for opponent.", True, (0, 0, 0))
-    counter = 0
     while not ws.is_connected:
         game.screen.fill(game.colors["main"])
         events = pygame.event.get()
         for event in events:
             game.event_handler(event)
-        if counter // 1000 == counter / 1000:
-            title = game.fonts["text"].render("Waiting for opponent.", True, (0, 0, 0))
-        if counter // 2000 == counter / 2000:
-            title = game.fonts["text"].render("Waiting for opponent...", True, (0, 0, 0))
-        if counter // 3000 == counter / 3000:
-            title = game.fonts["text"].render("Waiting for opponent.....", True, (0, 0, 0))
-        size_x, size_y = game.screen.get_size()
-        game.screen.blit(title, title.get_rect(center=(size_x / 2, size_y / 2)))
         game.coursor()
         pygame.display.update()
-        counter += 1
-
     player_move = ""
     fields = {
         "a": [None, None, None, None, None, None, None, None],
@@ -90,7 +69,6 @@ def run(game):
     cboard = CBoard()
     cd = ChessboardDrawer(game, ws.player_color)
     running = True
-
     while running:
         game.screen.fill(game.colors["main"])
         events = pygame.event.get()
@@ -105,27 +83,27 @@ def run(game):
                 if let and num and (let + str(num) != player_move) and ws.turn:
                     player_move += let + str(num)
             if event.type == VIDEORESIZE or event.type == VIDEOEXPOSE:
-                cd.resize()
-
+                cd.resize() 
         if ws.turn:
             if len(player_move) >= 4:
-                prom = ""
                 if chess.Move.from_uci(player_move) in ws.board.legal_moves or chess.Move.from_uci(player_move + "q") in ws.board.legal_moves:
-                    cboard.push(player_move)
-                    if cboard.is_promotion():
-                        prom = "q"
-                    player_move = player_move + prom
                     ws.send_move(player_move)
                     ws.turn = not ws.turn
-                    cboard.promote(prom)
+                    pygame.mixer.Sound.play(game.sound)
                 player_move = ""
         if len(ws.received_moves) > 0:
             for i in range(len(ws.received_moves)):
-                ws.board.push(chess.Move.from_uci(ws.received_moves[i][0]))
+                prom = ""
                 cboard.push(str(chess.Move.from_uci(ws.received_moves[i][0])))
+                if cboard.is_promotion():
+                    cboard.promote(prom)
+                    prom = "q"
+                ws.board.push(chess.Move.from_uci(ws.received_moves[i][0] + prom))
+                
                 if ws.received_moves[i][1] == ws.player_color:
                     ws.turn = True
                 del ws.received_moves[i]
+                pygame.mixer.Sound.play(game.sound)
         cd.draw(cboard, fields, (player_move if len(player_move) == 2 and ws.turn else False))
         game.coursor()
         pygame.display.update()
